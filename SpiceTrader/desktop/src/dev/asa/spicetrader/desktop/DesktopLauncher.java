17b41355d34b1df0203e9e10c4e552db65c66075
Asa <Asa@Kayes.lan>
2020-11-01 14:16:42 -0800

    initial commit

---
+++ SpiceTrader/desktop/src/dev/asa/spicetrader/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.asa.spicetrader.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.asa.spicetrader.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
