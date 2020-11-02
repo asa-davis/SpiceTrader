22baeb5f9cd0eb252c3a37faf02b574228037647
Asa <Asa@Kayes>
2020-11-02 15:47:52 -0800

    assets and texture packing

--- core/src/dev/asa/spicetrader/MainGame.java
+++ core/src/dev/asa/spicetrader/MainGame.java
@@ -5,20 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	AtlasRegion img;
+	TextureAtlas atlas;
 	
 	@Override
 	public void create () {
+		atlas = new TextureAtlas("assets/textures.atlas");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = atlas.findRegion("ships/player");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.05f, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
@@ -28,6 +32,6 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		atlas.dispose();
 	}
 }
--- desktop/src/dev/asa/spicetrader/desktop/DesktopLauncher.java
+++ desktop/src/dev/asa/spicetrader/desktop/DesktopLauncher.java
@@ -2,11 +2,32 @@ package dev.asa.spicetrader.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 import dev.asa.spicetrader.MainGame;
 
 public class DesktopLauncher {
+	
+	static float SCREEN_SIZE_RATIO = 3/4f;
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "Spice Trader";
+		config.width = (int)(1920 * SCREEN_SIZE_RATIO);
+		config.height = (int)(1080 * SCREEN_SIZE_RATIO);
+		config.resizable = true;
+		
+		// Pack all textures into a 'texture atlas'
+		TexturePacker.Settings sets = new TexturePacker.Settings();
+		sets.pot = true;
+		sets.fast = true;
+		sets.combineSubdirectories = true;
+		sets.paddingX = 1;
+		sets.paddingY = 1;
+		sets.edgePadding = true;
+		TexturePacker.process(sets, "raw_textures", "./assets", "textures");
+		
 		new LwjglApplication(new MainGame(), config);
 	}
 }
