6d0695fde153d15ccd8541b98334a80ec43646a7
Asa <Asa@Kayes.lan>
2020-11-07 16:26:27 -0800

    method in map to check for collisions

--- core/src/dev/asa/spicetrader/SpiceTraderMap.java
+++ core/src/dev/asa/spicetrader/SpiceTraderMap.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 
 public class SpiceTraderMap {
 	private int numCols;
@@ -35,6 +37,21 @@ public class SpiceTraderMap {
 	public void render(OrthographicCamera camera) {
 		this.mapRenderer.setView(camera);
 		this.mapRenderer.render();
+	}
+	
+	//returns true if ship is not intersecting with shore
+	//first we get the tile coords that the center of the hitbox is on
+	//then we check the tile Ids of those coords and the 8 neighboring tiles
+	//if any of these are land tiles (id != 0) we fetch their appropriate hitbox using the bitmask and check it for intersections with ship hitbox
+	public boolean validShipPosition(Polygon shipHitbox, Vector2 shipCenter) {
+		int[] currTile = this.getTileCoordsFromPixels(shipCenter);
+		List<Vector2> neighbors = Utils.getNeighborCoords(currTile[0], currTile[1], numCols, numRows, true);
+		return true;
+	}
+	
+	//takes a position in pixels and returns the tile coordinates
+	public int[] getTileCoordsFromPixels(Vector2 pos) {
+		return new int[] {(int) (pos.x/this.tileWidth), (int) (pos.y/this.tileHeight)};
 	}
 	
 	public void setTileIdMap(int[][] tileIdMap) {
