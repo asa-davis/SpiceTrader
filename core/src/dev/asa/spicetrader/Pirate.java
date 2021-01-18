d04e9820420df023baa7330f91cfee9e66865695
Asa Davis <asadavis1002@gmail.com>
2021-01-17 20:30:12 -0700

    refactored pirate player pathfinding for future wandering behavior

--- core/src/dev/asa/spicetrader/Pirate.java
+++ core/src/dev/asa/spicetrader/Pirate.java
@@ -15,6 +15,9 @@ public class Pirate extends Ship{
 	//keeps pirate from moving for a few frames after knocking player.
 	private int movementCooldown;
 	
+	//dijkstra map for finding path to player updated by map 
+	DijkstraMap pathToPlayer;
+	
 	//where the pirate is currently headed.
 	private Vector2 currGoal;
 	
@@ -22,8 +25,13 @@ public class Pirate extends Ship{
 	
 	public Pirate(Vector2 pos, Sprite sprite, SpiceTraderMap map, float initialDirection, PirateVillage base) {
 		super(pos, sprite, map, Utils.statToUse(DEFAULT_MAX_SPEED, 'm'), Utils.statToUse(DEFAULT_ACCEL, 'a'), Utils.statToUse(DEFAULT_TURNING, 't'), initialDirection, DEFAULT_HULL);
-		movementCooldown = 0;
+		
 		this.base = base;
+		
+		movementCooldown = 0;
+		
+		pathToPlayer = getMap().getPlayerDijkstraMap();
+		
 	}
 
 	@Override
@@ -36,9 +44,9 @@ public class Pirate extends Ship{
 		super.tick();
 		
 		//check if in chase range of player
-		if(getMap().getPlayerDijkstraMap().inRange(getHitCenter())) {
+		if(pathToPlayer.inRange(getHitCenter())) {
 			//get next move towards player
-			currGoal = getMap().getPlayerDijkstraMap().getNextMove(getHitCenter());
+			currGoal = pathToPlayer.getNextMove(getHitCenter());
 			
 			//check with entity manager that goal isnt shared by other pirates
 			currGoal = getManager().avoidOtherPirates(currGoal);
