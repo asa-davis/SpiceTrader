eb9a86cc7155a2bef2ad79bd00f0443ef958b7e6
Asa Davis <asadavis1002@gmail.com>
2021-09-13 00:38:30 -0600

    Make merchant match village

--- core/src/dev/asa/spicetrader/Merchant.java
+++ core/src/dev/asa/spicetrader/Merchant.java
@@ -4,10 +4,18 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class Merchant extends LandEntity {
     private ItemFactory itemFactory;
-    public Merchant(Vector2 pos, Sprite sprite, EntityFactory.LandEntityLocation location, Polygon dockHitbox, ItemFactory itemFactory)  {
+    private ArrayList<Item> toBuy = new ArrayList<>();
+    int tier;
+
+    public Merchant(Vector2 pos, Sprite sprite, EntityFactory.LandEntityLocation location, Polygon dockHitbox, ItemFactory itemFactory) {
         super(pos, sprite, location, dockHitbox);
         this.itemFactory = itemFactory;
+        this.tier = location.tier;
+        toBuy.add(itemFactory.getGinger());
+        toBuy.add(itemFactory.getCinnamon());
     }
 }
